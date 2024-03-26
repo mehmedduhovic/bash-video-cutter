7a05615cb0c8f2c9f7c901f726bb44f61a7a3162
Mehmed <mehmed_duhovic@hotmail.com>
2024-03-26 23:53:00 +0100

    additional work on the cutter script, error handling, fixing the freezing bug

--- video-short-creator-cutter.sh
+++ video-short-creator-cutter.sh
@@ -11,6 +11,9 @@ usage() {
 }
 
 start_script() {
+    input_video=""
+    input_timestamps=""
+    directory=""
     while [ $# -gt 0 ]; do
         case $1 in
         -h)
@@ -38,6 +41,28 @@ start_script() {
         esac
     done
 
+    error_count=0
+
+    if [ -z "$input_video" ]; then
+        echo "input video parameter is missing"
+        ((error_count++))
+    fi
+
+    if [ -z "$input_timestamps" ]; then
+        echo "input timestamps parameter is missing"
+        ((error_count++))
+    fi
+
+    if [ -z "$directory" ]; then
+        echo "output directory parameter is missing"
+        ((error_count++))
+    fi
+
+    if [ "$error_count" -gt 0 ]; then
+        echo "exiting the script"
+        exit 0
+    fi
+
     count=1
 
     while IFS= read -r line; do
@@ -47,11 +72,12 @@ start_script() {
 
         ffmpeg_cutting_opts=(
             -nostdin               #disable stdin, because ffmpeg runs as a background process using while
-            -i "$input_video"      #input file url
             -ss "$start_timestamp" #decodes the timestamp
             -to "$end_timestamp"   #stop writing the output
+            -i "$input_video"      #input file url
             -c:v copy              #codec video copy
             -c:a copy              #codec audio copy
+            -copyinkf              #fixes the freezing issue
             "$directory/$output_video"
         )
 
@@ -59,18 +85,18 @@ start_script() {
         ((count++))
     done <"$input_timestamps"
     cd "$directory" || exit
-    for file in *.mp4; do
-        file_name="${file%.*}"
-        new_file="${file_name}K.mp4"
+    # for file in *.mp4; do
+    #     file_name="${file%.*}"
+    #     new_file="${file_name}K.mp4"
 
-        ffmpeg_converting_to_portrait_opts=(
-            -i "$file"                                                                                                     #input file
-            -vf "scale='min(1080,iw)':min'(1920,ih)':force_original_aspect_ratio=decrease,pad=1080:1920:-1:-1:color=black" #filtergraph
-            "$new_file"
-        )
+    #     ffmpeg_converting_to_portrait_opts=(
+    #         -i "$file"                                                                                                     #input file
+    #         -vf "scale='min(1080,iw)':min'(1920,ih)':force_original_aspect_ratio=decrease,pad=1080:1920:-1:-1:color=black" #filtergraph
+    #         "$new_file"
+    #     )
 
-        ffmpeg "${ffmpeg_converting_to_portrait_opts[@]}"
-    done
+    #     ffmpeg "${ffmpeg_converting_to_portrait_opts[@]}"
+    # done
 }
 
 start_script "$@"
