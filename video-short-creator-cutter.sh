7c70561feff45a86b54885a1d8957905c874a7dc
Mehmed <mehmed_duhovic@hotmail.com>
2024-03-28 01:21:06 +0100

    upgraded error handling

--- video-short-creator-cutter.sh
+++ video-short-creator-cutter.sh
@@ -2,8 +2,8 @@
 
 # Function to display script usage
 usage() {
-    echo "Usage: $0 [OPTIONS]"
-    echo "Options:"
+    echo "Usage: $0 -i INPUT_VIDEO -t TIMESTAMP_FILE -o OUTPUT_DIRECTORY"
+    echo "List of possible arguments:"
     echo " -h   Display this help message"
     echo " -i   FILE Specify an input file"
     echo " -t   FILE Specify a timestamp file"
@@ -14,6 +14,7 @@ start_script() {
     input_video=""
     input_timestamps=""
     directory=""
+
     while [ $# -gt 0 ]; do
         case $1 in
         -h)
@@ -22,13 +23,13 @@ start_script() {
             ;;
         -i)
             input_video="$2"
-            echo "using $2 as input_video"
+            echo "using $2 as input video"
             shift
             shift
             ;;
         -t)
             input_timestamps="$2"
-            echo "using $2 as input_timestamps"
+            echo "using $2 as input timestamps"
             shift
             shift
             ;;
@@ -44,22 +45,33 @@ start_script() {
     error_count=0
 
     if [ -z "$input_video" ]; then
-        echo "input video parameter is missing"
+        echo -e "\033[31mERROR: Input video parameter is missing! \033[0m"
         ((error_count++))
     fi
 
     if [ -z "$input_timestamps" ]; then
-        echo "input timestamps parameter is missing"
+        echo -e "\033[31mERROR: Input timestamps parameter is missing! \033[0m"
         ((error_count++))
     fi
 
     if [ -z "$directory" ]; then
-        echo "output directory parameter is missing"
+        echo -e "\033[33mWARNING: Output directory parameter is missing! \033[0m"
+        echo "Creating the default directory named 'output'"
+        directory="output"
+        mkdir -p $directory
+    fi
+
+    if [[ ! -e "$directory" ]]; then
+        mkdir $directory
+    fi
+
+    if [[ ! -e "input_video" ]]; then
+        echo -e "\033[31mERROR: Input video is missing! \033[0m"
         ((error_count++))
     fi
 
     if [ "$error_count" -gt 0 ]; then
-        echo "exiting the script"
+        echo -e "\033[37;41mEXITING THE SCRIPT!\033[0m"
         exit 0
     fi
 
@@ -84,19 +96,21 @@ start_script() {
         ffmpeg "${ffmpeg_cutting_opts[@]}"
         ((count++))
     done <"$input_timestamps"
+
     cd "$directory" || exit
-    # for file in *.mp4; do
-    #     file_name="${file%.*}"
-    #     new_file="${file_name}K.mp4"
 
-    #     ffmpeg_converting_to_portrait_opts=(
-    #         -i "$file"                                                                                                     #input file
-    #         -vf "scale='min(1080,iw)':min'(1920,ih)':force_original_aspect_ratio=decrease,pad=1080:1920:-1:-1:color=black" #filtergraph
-    #         "$new_file"
-    #     )
+    for file in *.mp4; do
+        file_name="${file%.*}"
+        new_file="${file_name}K.mp4"
 
-    #     ffmpeg "${ffmpeg_converting_to_portrait_opts[@]}"
-    # done
+        ffmpeg_converting_to_portrait_opts=(
+            -i "$file"                                                                                                     #input file
+            -vf "scale='min(1080,iw)':min'(1920,ih)':force_original_aspect_ratio=decrease,pad=1080:1920:-1:-1:color=black" #filtergraph
+            "$new_file"
+        )
+
+        ffmpeg "${ffmpeg_converting_to_portrait_opts[@]}"
+    done
 }
 
 start_script "$@"
